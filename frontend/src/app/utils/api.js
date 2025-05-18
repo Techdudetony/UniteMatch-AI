3b340ceda093fb3414ab84f1004a141af4a690a2
Techdudetony <aalee2@student.fullsail.edu>
2025-05-18 19:21:00 -0400

    Update API Backend call

--- frontend/src/app/utils/api.js
+++ frontend/src/app/utils/api.js
@@ -27,7 +27,7 @@ export async function fetchTrainModel() {
 }
 
 export async function submitUserFeedback(feedbackPayload) {
-    const res = await fetch("http://127.0.0.1:8000/feedback", {
+    const res = await fetch(`${BASE_URL}/feedback`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(feedbackPayload),
